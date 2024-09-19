81dc020f1558a90ce12e2919caec1c6c86e98752
epicdigitalacademy <131464258+epicdigitalacademy@users.noreply.github.com>
2024-09-19 14:07:37 +0100

    Update Program.cs

--- ProductionCoreApp/Program.cs
+++ ProductionCoreApp/Program.cs
@@ -4,9 +4,6 @@ using OpenTelemetry.Metrics;
 using OpenTelemetry.Logs;
 using ProductionCoreApp.Models;
 using Microsoft.EntityFrameworkCore;
-using Serilog;
-using Serilog.Sinks.OpenTelemetry;
-using Serilog.Sinks.OpenTelemetry.Exporters;
 using MongoDB.Driver;
 using MongoDB.Driver.Core.Extensions.DiagnosticSources;
 using Npgsql;
@@ -16,78 +13,37 @@ using Confluent.Kafka.Extensions.OpenTelemetry;
 using Oracle.ManagedDataAccess.OpenTelemetry;
 
 
-
-
-
 namespace ProductionCoreApp
 {
-
     public class Program
     {
-        Action<ResourceBuilder> configureResource = resourceBuilder =>
-        {
-            var serviceName = "OneBankTransfer";
-            resourceBuilder.AddService(
-                serviceName: "MovieDev",
-                serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
-                serviceInstanceId: Environment.MachineName);
-        };
         public static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .CreateLogger();
-
-            try
-            {
-                Log.Information("Starting web application");
-
-                var builder = WebApplication.CreateBuilder(args);
-                builder.Services.AddSerilog(); // <-- Add this line
-
-
-        
-
-
-
-                builder.Services.AddOpenTelemetry() // Initialize the SDK
-           .ConfigureResource(ResourceBuilder =>
-            {
-               ResourceBuilder.AddService(
-                  serviceName: "OneBankTransfer",
-                   serviceInstanceId: Environment.MachineName);
-                   })
+            var builder = WebApplication.CreateBuilder(args);
+            var serviceName = "MovieDev";
+            builder.Services.AddOpenTelemetry() // Initialize the SDK   
             .WithTracing(builder => builder
             .AddAspNetCoreInstrumentation(options =>
              {
                  options.EnrichWithHttpRequest = (activity, httpRequest) =>
                   {
                       activity.SetTag("requestProtocol", httpRequest.Protocol);
-                      activity.SetTag("CorrelationId", httpRequest.Headers["CorrelationId"]);
                   };
                  options.RecordException = true;
                  options.EnrichWithHttpResponse = (activity, httpResponse) =>
                {
                    activity.SetTag("responseLength", httpResponse.ContentLength);
                };
-                 options.EnrichWithException = (activity, exception) =>
-                 {
-                     activity.SetTag("exceptionType", exception.GetType().ToString());
-                     activity.SetTag("stackTrace", exception.StackTrace);
-                 };
 
-             })
-
-                  .AddSource(nameof(Program))
+                })
+                  
                  .AddMongoDBInstrumentation()
                   .AddNpgsql()  // PostGres Opentelemetry Instrumentation
-                  .AddConfluentKafkaInstrumentation() // Kafka Opentelemetry Instrumentation
-
+                  .AddConfluentKafkaInstrumentation() // Kafka Opentelemetry Intrumentation
                  .AddSqlClientInstrumentation((options) =>  //
                  {
                      options.SetDbStatementForText = true;
                      options.SetDbStatementForStoredProcedure = true;
-                     options.RecordException = true;
                      options.EnableConnectionLevelAttributes = true;
                  })
                  .AddOracleDataProviderInstrumentation(options =>
@@ -97,119 +53,76 @@ namespace ProductionCoreApp
                      options.InstrumentOracleDataReaderRead = true;
                      options.SetDbStatementForText = true;
                  })
-
+                 
                 .AddHttpClientInstrumentation((options) =>
                  {
-                     options.EnrichWithHttpRequestMessage = (activity, httpRequestMessage) =>
-                     {
-                         activity.SetTag("requestVersion", httpRequestMessage.Version);
-                     };
-                     options.EnrichWithHttpResponseMessage = (activity, httpResponseMessage) =>
-                     {
-                         activity.SetTag("requestVersion", httpResponseMessage.Version);
-                     };
-                     options.EnrichWithException = (activity, exception) =>
-                     {
-                         activity.SetTag("stackTrace", exception.StackTrace);
-                     };
+                     options.EnrichWithHttpRequestMessage = (activity, httpRequestMessage) => activity.SetTag("requestVersion",
+                     httpRequestMessage.Version);
+                     options.EnrichWithHttpResponseMessage = (activity, httpResponseMessage) => activity.SetTag("requestVersion",
+                     httpResponseMessage.Version);
+                     options.EnrichWithException = (activity, exception) => activity.SetTag("stackTrace", exception.StackTrace);
                  })
-
-                 .AddConsoleExporter()
+                   .AddSource(serviceName)
+                .SetResourceBuilder(
+                    ResourceBuilder.CreateDefault()
+                        .AddService(serviceName: serviceName))
+                .AddConsoleExporter()
                  .AddOtlpExporter());
 
-                builder.Services.AddOpenTelemetry()
-                 .ConfigureResource(ResourceBuilder =>
-                 {
-                     ResourceBuilder.AddService(
-                         serviceName: "OneBankTransfer",
-                         serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
-                         serviceInstanceId: Environment.MachineName);
-                 })
+            builder.Services.AddOpenTelemetry()
+             .WithMetrics(builder => builder
+                 .SetResourceBuilder(
+                ResourceBuilder.CreateDefault()
+                .AddService(serviceName: "MovieDev"))
+             .AddAspNetCoreInstrumentation()
+             .AddRuntimeInstrumentation()
+             .AddProcessInstrumentation()
+             .AddConsoleExporter() 
+             .AddOtlpExporter());
 
-                 .WithMetrics(builder => builder
-                     .SetResourceBuilder(
-                    ResourceBuilder.CreateDefault()
-                    .AddService(serviceName: "OneBankTransfer"))
-                    .AddAspNetCoreInstrumentation()
-                    .AddRuntimeInstrumentation()
-                    .AddProcessInstrumentation()
-                    .AddView(
-                     "kafka_consumer_event_duration",
-                    new ExplicitBucketHistogramConfiguration
+
+            builder.Logging.AddOpenTelemetry(options =>
+            {
+                options.SetResourceBuilder(
+            ResourceBuilder
+                .CreateDefault()
+                .AddService(
+                    serviceName: "MovieDev",
+                    serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+                    serviceInstanceId: Environment.MachineName));
+                options.IncludeFormattedMessage = true;
+                options.IncludeScopes = true;
+                options.ParseStateValues = true;
+                options.AddConsoleExporter();
+                options.AddOtlpExporter();
+            });
+
+
+            // Add services to the container.
+            builder.Services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext")));
+
+                    builder.Services.AddControllers();
+                    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                    builder.Services.AddEndpointsApiExplorer();
+                    builder.Services.AddSwaggerGen();
+
+                    var app = builder.Build();
+
+                    // Configure the HTTP request pipeline.
+                   //if (app.Environment.IsDevelopment())
                     {
-                        Boundaries = new[]
-                        {
-                            0,
-                            0.005,
-                            0.01,
-                            // ...
-                            10
-                        }
-                    })
-                .AddMeter(
-                    "System.Runtime",
-                    "Microsoft.AspNetCore.Hosting",
-                    "Microsoft.AspNetCore.Server.Kestrel"
-                    )
+                        app.UseSwagger();
+                        app.UseSwaggerUI();
+                    }
 
-                      .AddConsoleExporter()
-                    .AddOtlpExporter());
-
-                builder.Logging.AddOpenTelemetry(options =>
-                {
-                    options.SetResourceBuilder(
-                ResourceBuilder
-                    .CreateDefault()
-                    .AddService(
-                        serviceName: "OneBankTransfer",
-                        serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
-                        serviceInstanceId: Environment.MachineName));
-                    options.IncludeFormattedMessage = true;
-                    options.IncludeScopes = true;
-                    options.ParseStateValues = true;
-                    options.AddConsoleExporter();
-                    options.AddOtlpExporter();
-                });
+                    app.UseHttpsRedirection();
+                    app.UseAuthorization();
 
 
+                    app.MapControllers();
 
-
-                // Add services to the container.
-                builder.Services.AddDbContext<MovieContext>(options =>
-                    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext")));
-
-                builder.Services.AddControllers();
-                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-                builder.Services.AddEndpointsApiExplorer();
-                builder.Services.AddSwaggerGen();
-                builder.Services.AddSerilog(); // <-- Add this line
-                var app = builder.Build();
-
-                // Configure the HTTP request pipeline.
-                //if (app.Environment.IsDevelopment())
-                {
-                    app.UseSwagger();
-                    app.UseSwaggerUI();
+                    app.Run();
                 }
-
-                app.UseHttpsRedirection();
-                app.UseAuthorization();
-
-
-                app.MapControllers();
-
-                app.Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Application terminated unexpectedly");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-
-            }
         }
     }
-}
-
